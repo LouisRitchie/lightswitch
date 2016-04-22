ac9c49f32d1b6de9f905f8313b38f5f86216abe5
LouisRitchie <louiscritchie@gmail.com>
2016-04-22 00:15:55 -0700

    Fixed webpack config script errors, changed start script in package.json, updated gitignore :metal:

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 const merge = require('webpack-merge');
 const TARGET = process.env.npm_lifecycle_event;
 
@@ -7,6 +7,8 @@ const PATHS = {
     build: path.join(__dirname, 'build')
 }
 
+const webpack = require('webpack');
+
 const common = {
     entry: {
         app: PATHS.app
@@ -14,14 +16,48 @@ const common = {
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                // Include accepts either a path or an array of paths
+                include: PATHS.app
+            }   
+        ]
     }
 };
 
 // default configuration
 if(TARGET === 'start' || !TARGET) {
-    module.exports = merge(common, {});    
+    module.exports = merge(common, {
+        devServer: {
+            contentBase: PATHS.build,
+
+            // enable API history fallback so HTML5 History API based routing
+            // works. This is a good default that will come in handy in more
+            // complicated setups.
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
+
+            // Parse host and port from env so this is easy to customize.
+            //
+            // localhost
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
 }
 
 if(TARGET === 'build') {
-    module.exports - merge(common, {});    
+    module.exports = merge(common, {});    
 }
