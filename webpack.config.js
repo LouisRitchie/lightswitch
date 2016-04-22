91bf17ea14f011680e2eae1127859ae8ceb979f1
LouisRitchie <louiscritchie@gmail.com>
2016-04-22 01:24:19 -0700

    :smile_cat: Babel added and configured, ready for react :punch:

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,12 @@ const common = {
     entry: {
         app: PATHS.app
     },
+    // add resolve.extensions.
+    // '' is needed to allow imports without an extension.
+    // Note the .'s before extensions as it will fail to match without!!!
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
     output: {
         path: PATHS.build,
         filename: 'bundle.js'
@@ -25,7 +31,18 @@ const common = {
                 loaders: ['style', 'css'],
                 // Include accepts either a path or an array of paths
                 include: PATHS.app
-            }   
+            },
+            {
+                test: /\.jsx?$/,
+                // Enable caching for improved performance during development
+                // It uses default OS directory by default. If you need something
+                // more custom, pass a path to it. IE babel?cacheDirectory=<path>
+                loaders: ['babel?cacheDirectory'],
+                // Parse only app files! Without this it will go through the entire
+                // project. In addition to being slow, that will most likely
+                // result in an error!
+                include: PATHS.app
+            }
         ]
     }
 };
